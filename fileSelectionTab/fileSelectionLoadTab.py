df8292a70dfd1c17be5b9a4cd75a0519e36bcd7f
Robin Hofmann <hofmann.robin@web.de>
2021-08-31 12:39:10 +0200

    moved file load tab into own class

---
+++ fileSelectionTab/fileSelectionLoadTab.py
@@ -0,0 +1,114 @@
+import os
+import tkinter as tk
+import tkinter.messagebox
+
+from imageFileManager import ImageFileManager
+from tooltips import BindTooltip
+
+
+class FileSelectionLoadTab:
+    """ When clicking the load button, not saving/loading related """
+
+    def __init__(self, aoeGui, overlayPanel, parentFrame,
+                 imageFolderVar,
+                 separateMaskFolderVar, separateMaskFolderPathVar,
+                 ):
+        self._aoeGUI = aoeGui
+        self._overlayPanel = overlayPanel
+        self._parentFrame = parentFrame
+
+        self._imageFolderVar = imageFolderVar
+        self._separateMaskFolderVar = separateMaskFolderVar
+        self._separateMaskFolderPathVar = separateMaskFolderPathVar
+        self._buildUI()
+
+    def _buildUI(self):
+        self._loadPanelFrame = tk.LabelFrame(self._parentFrame, text=" ")
+        self._loadPanelFrame.grid_columnconfigure(0, weight=1)
+        self._loadPanelFrame.grid_rowconfigure(5, weight=1)
+        loadButton = tk.Button(self._loadPanelFrame, text="Load", command=self._loadIntoOverlayTab)
+
+        self._imagesLoadedVar = tk.StringVar()
+        self._masksLoadedVar = tk.StringVar()
+        self._overlayImagesVar = tk.StringVar()
+        self._loadSummaryVar = tk.StringVar()
+
+        imagesLoadedLabel = tk.Label(self._loadPanelFrame, textvariable=self._imagesLoadedVar, width=10, anchor="w")
+        masksLoadedLabel = tk.Label(self._loadPanelFrame, textvariable=self._masksLoadedVar, width=10, anchor="w")
+        overlayImagesLabel = tk.Label(self._loadPanelFrame, textvariable=self._overlayImagesVar, width=10, anchor="w")
+        loadSummaryLabel = tk.Label(self._loadPanelFrame, textvariable=self._loadSummaryVar, width=10, anchor="center")
+        self._loadSummaryDetailTextArea = tk.Text(self._loadPanelFrame, width=10)
+
+        loadButton.grid(row=0, column=0, pady=(10, 5))
+        imagesLoadedLabel.grid(row=1, column=0, sticky="news", pady=(10, 5))
+        masksLoadedLabel.grid(row=2, column=0, sticky="news", pady=(10, 5))
+        overlayImagesLabel.grid(row=3, column=0, sticky="news", pady=(10, 5))
+        loadSummaryLabel.grid(row=4, column=0, sticky="news", pady=(5, 5))
+        self._loadSummaryDetailTextArea.grid(row=5, column=0, sticky="news", pady=(5, 5), padx=(5, 5))
+        BindTooltip(self._loadSummaryDetailTextArea, "Shows issues after paths have been loaded")
+
+    def getFrame(self):
+        return self._loadPanelFrame
+
+
+    def _loadIntoOverlayTab(self):
+        if self._separateMaskFolderVar.get():
+            maskFolderPath = self._separateMaskFolderPathVar.get()
+        else:
+            maskFolderPath = self._imageFolderVar.get()
+
+        if not os.path.exists(self._imageFolderVar.get()):
+            tk.messagebox.showwarning("No image folder", "You need to set an image folder path")
+            return
+
+        self._fileManager = ImageFileManager(self._imageFolderVar.get(), maskFolderPath,
+                                       self._overlayPanel.getOverlayImages())
+        consistencyCheck = self._fileManager.checkFileToMapConsitency()
+        if not self.setStatusMessagesAfterLoad(self._fileManager, consistencyCheck):
+            return
+
+        self._aoeGUI.getOverlayElement().ImageFileManager = self._fileManager
+
+        self._aoeGUI.getOverlayElement().loadConfiguration(
+            self._imageFolderVar.get(), maskFolderPath,
+            self._overlayPanel.getOverlayImages()
+        )
+
+    def setStatusMessagesAfterLoad(self, fileManager, consistencyCheck) -> bool:
+        noIssues = fileManager.evaluateCheckResult(consistencyCheck)
+
+        imagesLoaded = len(fileManager.getAllImageNames())
+
+        imagesLoadedMsg = f"Images loaded: \t\t{imagesLoaded}"
+        self._imagesLoadedVar.set(imagesLoadedMsg)
+        masksLoadedMsg = f"Masks loaded: \t\t{len(fileManager.getAllMaskNames())}"
+        self._masksLoadedVar.set(masksLoadedMsg)
+        overlaysLoadedMsg = f"Overlay images load: \t{len(self._overlayPanel.getOverlayImages().keys())}"
+        self._overlayImagesVar.set(overlaysLoadedMsg)
+
+        if noIssues:
+            if imagesLoaded == 0:
+                self._loadSummaryVar.set("Nothing loaded...")
+                return False
+            else:
+                self._loadSummaryVar.set("No issues found in loading")
+        else:
+            self._loadSummaryVar.set("Missing images detected!")
+
+        self._loadSummaryDetailTextArea.delete("1.0", tk.END)
+        outputFolder = self._aoeGUI.outputFolderPathVar.get()
+        if not outputFolder or not os.path.exists(outputFolder):
+            self._loadSummaryDetailTextArea.insert(tk.END, f"Missing output folder\n\n")
+
+        if not consistencyCheck[2]:
+            self._loadSummaryDetailTextArea.insert(tk.END, "No overlay images loaded\n\n")
+        if len(consistencyCheck[0]) != 0:
+            missingMasks = "\n> ".join(consistencyCheck[0])
+            self._loadSummaryDetailTextArea.insert(tk.END, f"No masks found for images:\n {missingMasks}\n\n")
+            print(f"[WARN] Images missing masks: {consistencyCheck[0]}")
+        if len(consistencyCheck[1]) != 0:
+            missingImages = "\n> ".join(consistencyCheck[1])
+            self._loadSummaryDetailTextArea.insert(tk.END, f"No images found for masks:\n {missingImages}\n\n")
+            print(f"[WARN] Masks missing images: {consistencyCheck[1]}")
+
+        return True
--- fileSelectionTab/fileSelectionTab.py
+++ fileSelectionTab/fileSelectionTab.py
@@ -4,6 +4,7 @@ import tkinter.filedialog
 import tkinter.messagebox
 import tkinter.ttk as ttk
 
+from fileSelectionTab.fileSelectionLoadTab import FileSelectionLoadTab
 from fileSelectionTab.overlayImageSelection import OverlayImageSelectionPanel
 from imageFileManager import ImageFileManager
 from tooltips import BindTooltip
@@ -39,26 +40,26 @@ class FileSelectionTab:
         self._introFrame.grid(row=0, column=0, columnspan=2, sticky="news", padx=(5, 5), pady=(5, 5))
 
     def _buildSelectionPanel(self):
-        selectionPanel = tk.LabelFrame(self._parentFrame, text="Folder Selection")
-        selectionPanel.grid(row=1, column=0, sticky="news", padx=(5, 5), pady=(5, 5))
-        selectionPanel.grid_columnconfigure(0, weight=0)
-        selectionPanel.grid_columnconfigure(1, weight=1)
-        selectionPanel.grid_columnconfigure(2, weight=0)
+        self._selectionPanel = tk.LabelFrame(self._parentFrame, text="Folder Selection")
+        self._selectionPanel.grid(row=1, column=0, sticky="news", padx=(5, 5), pady=(5, 5))
+        self._selectionPanel.grid_columnconfigure(0, weight=0)
+        self._selectionPanel.grid_columnconfigure(1, weight=1)
+        self._selectionPanel.grid_columnconfigure(2, weight=0)
 
         self._imageFolderVar = tk.StringVar()
-        imageFolderLabel = tk.Label(selectionPanel, text="Image folder: ")
-        imageFolderEntry = tk.Entry(selectionPanel, textvariable=self._imageFolderVar)
-        imageFolderSelectionButton = tk.Button(selectionPanel, image=self._folderIcon,
+        imageFolderLabel = tk.Label(self._selectionPanel, text="Image folder: ")
+        imageFolderEntry = tk.Entry(self._selectionPanel, textvariable=self._imageFolderVar)
+        imageFolderSelectionButton = tk.Button(self._selectionPanel, image=self._folderIcon,
                                                command=lambda: setFolderPath(self._imageFolderVar,
                                                                              "Select image folder"))
 
-        outputFolderLabel = tk.Label(selectionPanel, text="Output folder: ")
-        outputFolderEntry = tk.Entry(selectionPanel, textvariable=self._aoeGUI.outputFolderPathVar)
-        outputFolderSelectionButton = tk.Button(selectionPanel, image=self._folderIcon,
+        outputFolderLabel = tk.Label(self._selectionPanel, text="Output folder: ")
+        outputFolderEntry = tk.Entry(self._selectionPanel, textvariable=self._aoeGUI.outputFolderPathVar)
+        outputFolderSelectionButton = tk.Button(self._selectionPanel, image=self._folderIcon,
                                                 command=lambda: setFolderPath(self._aoeGUI.outputFolderPathVar,
                                                                               "Select output folder"))
 
-        maskFolderPanel = tk.LabelFrame(selectionPanel, text="Image masks")
+        maskFolderPanel = tk.LabelFrame(self._selectionPanel, text="Image masks")
         maskFolderPanel.grid_columnconfigure(1, weight=1)
         self._separateMaskFolderVar = tk.BooleanVar()
         self._separateMaskFolderPathVar = tk.StringVar()
@@ -72,7 +73,7 @@ class FileSelectionTab:
                                                     command=lambda: setFolderPath(self._separateMaskFolderPathVar,
                                                                                   "Select separate mask folder"))
 
-        self._overlayPanel = OverlayImageSelectionPanel(self._aoeGUI, selectionPanel)
+        self._overlayPanel = OverlayImageSelectionPanel(self._aoeGUI, self._selectionPanel)
 
         imageFolderLabel.grid(row=0, column=0)
         imageFolderEntry.grid(row=0, column=1, sticky="ew", padx=(5, 5))
@@ -92,99 +93,15 @@ class FileSelectionTab:
         self._overlayPanel.getFrame().grid(row=3, column=0, columnspan=3, sticky="news", pady=(10, 10), padx=(5, 5))
 
     def _buildButtonPanel(self):
-        buttonPanel = tk.LabelFrame(self._parentFrame, text=" ")
-        buttonPanel.grid_columnconfigure(0, weight=1)
-        buttonPanel.grid_rowconfigure(5, weight=1)
-        loadButton = tk.Button(buttonPanel, text="Load", command=self._loadIntoOverlayTab)
-
-        self._imagesLoadedVar = tk.StringVar()
-        self._masksLoadedVar = tk.StringVar()
-        self._overlayImagesVar = tk.StringVar()
-        self._loadSummaryVar = tk.StringVar()
-
-        imagesLoadedLabel = tk.Label(buttonPanel, textvariable=self._imagesLoadedVar, width=10, anchor="w")
-        masksLoadedLabel = tk.Label(buttonPanel, textvariable=self._masksLoadedVar, width=10, anchor="w")
-        overlayImagesLabel = tk.Label(buttonPanel, textvariable=self._overlayImagesVar, width=10, anchor="w")
-        loadSummaryLabel = tk.Label(buttonPanel, textvariable=self._loadSummaryVar, width=10, anchor="center")
-        self._loadSummaryDetailTextArea = tk.Text(buttonPanel, width=10)
-
-        loadButton.grid(row=0, column=0, pady=(10, 5))
-        imagesLoadedLabel.grid(row=1, column=0, sticky="news", pady=(10, 5))
-        masksLoadedLabel.grid(row=2, column=0, sticky="news", pady=(10, 5))
-        overlayImagesLabel.grid(row=3, column=0, sticky="news", pady=(10, 5))
-        loadSummaryLabel.grid(row=4, column=0, sticky="news", pady=(5, 5))
-        self._loadSummaryDetailTextArea.grid(row=5, column=0, sticky="news", pady=(5, 5), padx=(5, 5))
-        BindTooltip(self._loadSummaryDetailTextArea, "Shows issues after paths have been loaded")
-
-        buttonPanel.grid(row=1, column=1, sticky="news", pady=(5, 5), padx=(5, 5))
+        self._loadTab = FileSelectionLoadTab(self._aoeGUI, self._overlayPanel, self._parentFrame,
+                                             self._imageFolderVar,
+                                             self._separateMaskFolderVar, self._separateMaskFolderPathVar)
+        self._loadTab.getFrame().grid(row=1, column=1, sticky="news", pady=(5, 5), padx=(5, 5))
 
     def _toggleIndividualMaskFolder(self):
         useIndividualFolder = self._separateMaskFolderVar.get()
         tkState = tk.NORMAL if useIndividualFolder else tk.DISABLED
         self._maskFolderSelectionButton.config(state=tkState)
-
-    def _loadIntoOverlayTab(self):
-        if self._separateMaskFolderVar.get():
-            maskFolderPath = self._separateMaskFolderPathVar.get()
-        else:
-            maskFolderPath = self._imageFolderVar.get()
-
-        if not os.path.exists(self._imageFolderVar.get()):
-            tk.messagebox.showwarning("No image folder", "You need to set an image folder path")
-            return
-
-        self._fileManager = ImageFileManager(self._imageFolderVar.get(), maskFolderPath,
-                                       self._overlayPanel.getOverlayImages())
-        consistencyCheck = self._fileManager.checkFileToMapConsitency()
-        if not self.setStatusMessagesAfterLoad(self._fileManager, consistencyCheck):
-            return
-
-        self._aoeGUI.getOverlayElement().ImageFileManager = self._fileManager
-
-        self._aoeGUI.getOverlayElement().loadConfiguration(
-            self._imageFolderVar.get(), maskFolderPath,
-            self._overlayPanel.getOverlayImages()
-        )
-
-    def setStatusMessagesAfterLoad(self, fileManager, consistencyCheck) -> bool:
-        consistencyCheck = fileManager.checkFileToMapConsitency()
-        noIssues = fileManager.evaluateCheckResult(consistencyCheck)
-
-        imagesLoaded = len(fileManager.getAllImageNames())
-
-        imagesLoadedMsg = f"Images loaded: \t\t{imagesLoaded}"
-        self._imagesLoadedVar.set(imagesLoadedMsg)
-        masksLoadedMsg = f"Masks loaded: \t\t{len(fileManager.getAllMaskNames())}"
-        self._masksLoadedVar.set(masksLoadedMsg)
-        overlaysLoadedMsg = f"Overlay images load: \t{len(self._overlayPanel.getOverlayImages().keys())}"
-        self._overlayImagesVar.set(overlaysLoadedMsg)
-
-        if noIssues:
-            if imagesLoaded == 0:
-                self._loadSummaryVar.set("Nothing loaded...")
-                return False
-            else:
-                self._loadSummaryVar.set("No issues found in loading")
-        else:
-            self._loadSummaryVar.set("Missing images detected!")
-
-        self._loadSummaryDetailTextArea.delete("1.0", tk.END)
-        outputFolder = self._aoeGUI.outputFolderPathVar.get()
-        if not outputFolder or not os.path.exists(outputFolder):
-            self._loadSummaryDetailTextArea.insert(tk.END, f"Missing output folder\n\n")
-
-        if not consistencyCheck[2]:
-            self._loadSummaryDetailTextArea.insert(tk.END, "No overlay images loaded\n\n")
-        if len(consistencyCheck[0]) != 0:
-            missingMasks = "\n> ".join(consistencyCheck[0])
-            self._loadSummaryDetailTextArea.insert(tk.END, f"No masks found for images:\n {missingMasks}\n\n")
-            print(f"[WARN] Images missing masks: {consistencyCheck[0]}")
-        if len(consistencyCheck[1]) != 0:
-            missingImages = "\n> ".join(consistencyCheck[1])
-            self._loadSummaryDetailTextArea.insert(tk.END, f"No images found for masks:\n {missingImages}\n\n")
-            print(f"[WARN] Masks missing images: {consistencyCheck[1]}")
-
-        return True
 
     def serialiseState(self):
         return {"imageFolder": self._imageFolderVar.get(),
